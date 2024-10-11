9080e51215adc461a7ed16036a93f0cf9565ffc8
Cuong Ngo Quoc <141616567+quethanhhoa36@users.noreply.github.com>
2024-10-11 20:26:57 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
+const usersRouter = require("./app/routes/user.route")
 
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/user', usersRouter);
 app.use('/', (req,res)=>{
     res.json({message: "welcome to bookstore"});
 })
