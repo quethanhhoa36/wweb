98a15ad549f7e6bbebc435a286fbe0190bf6961f
Cuong Ngo Quoc <141616567+quethanhhoa36@users.noreply.github.com>
2024-10-11 20:35:34 +0700

    Update user.controller.js

---
+++ app/controllers/user.controller.js
@@ -0,0 +1,18 @@
+exports.create = (req,res) => {
+    res.send({message: "create"});
+}
+exports.findAll = (req,res) => {
+    res.send({message: "create"});
+}
+exports.findOne = (req,res) => {
+    res.send({message: "create"});
+}
+exports.update = (req,res) => {
+    res.send({message: "create"});
+}
+exports.delete = (req,res) => {
+    res.send({message: "create"});
+}
+exports.deleteAll = (req,res) => {
+    res.send({message: "create"});
+}
