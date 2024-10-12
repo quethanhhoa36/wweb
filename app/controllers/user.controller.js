fa2bd9a2abe0c136e527ea4d295de6acd4c37afb
Cuong Ngo Quoc <141616567+quethanhhoa36@users.noreply.github.com>
2024-10-12 08:35:21 +0700

    Update user.controller.js

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,8 +1,28 @@
-exports.create = (req,res) => {
-    res.send({message: "create"});
+const ApiError = require('../api-error')
+const UserService =require('../services/user.service');
+const MongoDB = require("../utils/mongodb.util");
+
+exports.create = async (req,res,next)=>{
+    if(!req.body?.name){
+
+        return next(new ApiError(400,"Name cannot be empty"));
+    }
+    try{
+        const userService = new UserService(MongoDB.client);
+        const documents = await userService.create(req.body);
+        return res.send(documents);
+    }
+    catch(error){
+        return next(
+            new ApiError(500,"An error occured while creating contact")
+        );
+    }
 }
-exports.findAll = (req,res) => {
-    res.send({message: "create"});
+exports.findAll = async (req,res,next) => {
+    let documents = [];
+    const userService = new UserService(MongoDB.client);
+    documents = await userService.find({});
+    return res.send(documents);
 }
 exports.findOne = (req,res) => {
     res.send({message: "create"});
